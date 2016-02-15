51fb7f0d85ee2cde4e5ddbb231de53200ebec1d0
Sandro Neumann <sandro.neumann@idgis.nl>
2016-02-15 14:51:52 +0100

    Rebase and solved merge conflicts

--- geoportaal-beheer/app/controllers/Index.java
+++ geoportaal-beheer/app/controllers/Index.java
@@ -1,11 +1,23 @@
 package controllers;
 
+import static models.QCreator.creator;
+import static models.QCreatorLabel.creatorLabel;
+import static models.QMdAttachment.mdAttachment;
 import static models.QMdFormat.mdFormat;
 import static models.QMdFormatLabel.mdFormatLabel;
+import static models.QMdSubject.mdSubject;
 import static models.QMetadata.metadata;
+import static models.QRights.rights;
+import static models.QRightsLabel.rightsLabel;
 import static models.QStatus.status;
 import static models.QStatusLabel.statusLabel;
+import static models.QSubject.subject;
+import static models.QSubjectLabel.subjectLabel;
 import static models.QSupplier.supplier;
+import static models.QTypeInformation.typeInformation;
+import static models.QTypeInformationLabel.typeInformationLabel;
+import static models.QUseLimitation.useLimitation;
+import static models.QUseLimitationLabel.useLimitationLabel;
 import static models.QUser.user;
 
 import java.sql.SQLException;
@@ -19,6 +31,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.sql.SQLQuery;
 
 import actions.DefaultAuthenticator;
@@ -92,23 +105,24 @@ public class Index extends Controller {
 						status.name, mdFormat.name)
 		    			.from(metadata)
 		    			.join(mdAttachment).on(metadata.id.eq(mdAttachment.metadataId))
-    			.join(typeInformation).on(metadata.typeInformation.eq(typeInformation.id))
-    			.join(typeInformationLabel).on(typeInformation.id.eq(typeInformationLabel.id))
-    			.join(creator).on(metadata.creator.eq(creator.id))
-    			.join(creatorLabel).on(creator.id.eq(creatorLabel.id))
-    			.join(rights).on(metadata.rights.eq(rights.id))
-    			.join(rightsLabel).on(rights.id.eq(rightsLabel.id))
-    			.join(useLimitation).on(metadata.useLimitation.eq(useLimitation.id))
-    			.join(useLimitationLabel).on(useLimitation.id.eq(useLimitationLabel.id))
-    			.join(mdFormat).on(metadata.mdFormat.eq(mdFormat.id))
-    			.join(mdFormatLabel).on(mdFormat.id.eq(mdFormatLabel.id))
-    			.join(mdSubject).on(metadata.id.eq(mdSubject.metadataId))
-    			.join(subject).on(mdSubject.subject.eq(subject.id))
-    			.join(subjectLabel).on(subject.id.eq(subjectLabel.id))
-    			.join(status).on(metadata.status.eq(status.id))
-    			.join(statusLabel).on(status.id.eq(statusLabel.statusId))
-    			.join(supplier).on(metadata.supplier.eq(supplier.id));
-		
+		    			.join(typeInformation).on(metadata.typeInformation.eq(typeInformation.id))
+		    			.join(typeInformationLabel).on(typeInformation.id.eq(typeInformationLabel.id))
+		    			.join(creator).on(metadata.creator.eq(creator.id))
+		    			.join(creatorLabel).on(creator.id.eq(creatorLabel.id))
+		    			.join(rights).on(metadata.rights.eq(rights.id))
+		    			.join(rightsLabel).on(rights.id.eq(rightsLabel.id))
+		    			.join(useLimitation).on(metadata.useLimitation.eq(useLimitation.id))
+		    			.join(useLimitationLabel).on(useLimitation.id.eq(useLimitationLabel.id))
+		    			.join(mdFormat).on(metadata.mdFormat.eq(mdFormat.id))
+		    			.join(mdFormatLabel).on(mdFormat.id.eq(mdFormatLabel.id))
+		    			.join(mdSubject).on(metadata.id.eq(mdSubject.metadataId))
+		    			.join(subject).on(mdSubject.subject.eq(subject.id))
+		    			.join(subjectLabel).on(subject.id.eq(subjectLabel.id))
+		    			.join(status).on(metadata.status.eq(status.id))
+		    			.join(statusLabel).on(status.id.eq(statusLabel.statusId))
+		    			.join(supplier).on(metadata.supplier.eq(supplier.id));
+	        	
+	        	/*
 				SQLQuery<Tuple> fullTextQuery = tx.select(metadata.id,
 					Expressions.stringTemplate("to_tsvector('dutch', {0}) || "
 						+ "to_tsvector('dutch', coalesce((string_agg({1}, ' ')), '') || "
@@ -149,6 +163,7 @@ public class Index extends Controller {
 				for(Tuple searchField : searchRecord) {
 					System.out.println(searchField);
 				}
+				*/
 				
 				if(!"none".equals(supplierSearch)) {
 					datasetQuery
@@ -438,177 +453,6 @@ public class Index extends Controller {
 		}
 	}
 	
-<<<<<<< Upstream, based on origin/master
-=======
-	public Result search() {
-		Form<Search> searchForm = Form.form(Search.class);
-		Search s = searchForm.bindFromRequest().get();
-		String textSearch = s.getText();
-		String supplierSearch = s.getSupplier();
-		String statusSearch = s.getStatus();
-		String mdFormatSearch = s.getFormat();
-		Date dateStartSearch = s.getDateUpdateStart();
-		Date dateEndSearch = s.getDateUpdateEnd();
-		
-		return q.withTransaction(tx -> {
-			SQLQuery<Tuple> datasetQuery = tx.select(metadata.id, metadata.uuid, metadata.title, metadata.lastRevisionDate, statusLabel.label, supplier.name, 
-				status.name, mdFormat.name)
-	    		.from(metadata)
-	    		.join(mdAttachment).on(metadata.id.eq(mdAttachment.metadataId))
-    			.join(typeInformation).on(metadata.typeInformation.eq(typeInformation.id))
-    			.join(typeInformationLabel).on(typeInformation.id.eq(typeInformationLabel.id))
-    			.join(creator).on(metadata.creator.eq(creator.id))
-    			.join(creatorLabel).on(creator.id.eq(creatorLabel.id))
-    			.join(rights).on(metadata.rights.eq(rights.id))
-    			.join(rightsLabel).on(rights.id.eq(rightsLabel.id))
-    			.join(useLimitation).on(metadata.useLimitation.eq(useLimitation.id))
-    			.join(useLimitationLabel).on(useLimitation.id.eq(useLimitationLabel.id))
-    			.join(mdFormat).on(metadata.mdFormat.eq(mdFormat.id))
-    			.join(mdFormatLabel).on(mdFormat.id.eq(mdFormatLabel.id))
-    			.join(mdSubject).on(metadata.id.eq(mdSubject.metadataId))
-    			.join(subject).on(mdSubject.subject.eq(subject.id))
-    			.join(subjectLabel).on(subject.id.eq(subjectLabel.id))
-    			.join(status).on(metadata.status.eq(status.id))
-    			.join(statusLabel).on(status.id.eq(statusLabel.statusId))
-    			.join(supplier).on(metadata.supplier.eq(supplier.id));
-			
-			String[] textSearchArray = textSearch.split(" ");
-		List<String> textSearchList = new ArrayList<String>();
-		for(String word : textSearchArray) {
-			if(!word.isEmpty()) {
-				textSearchList.add(word.toLowerCase());
-			}
-		}
-		
-		/*
-		 .select(metadata.all())
-					.from(metadata)
-					.where(metadata.id.eq(
-							SQLExpressions.select(metadata3.id.max()).from(metadata3)))
-		 */
-		
-		SQLQuery<Tuple> fullTextQuery = db.queryFactory
-					.select(metadata.id,
-							Expressions.stringTemplate("to_tsvector('dutch', {0}) || "
-								+ "to_tsvector('dutch', coalesce((string_agg({1}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({2}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({3}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({4}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({5}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({6}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({7}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({8}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({9}, ' ')), '') ||"
-								+ "to_tsvector('dutch', coalesce((string_agg({10}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({11}, ' ')), '') || "
-								+ "to_tsvector('dutch', coalesce((string_agg({12}, ' ')), '') ||"
-								+ "to_tsvector('dutch', coalesce((string_agg({13}, ' ')), ''))",
-								metadata.title, metadata.description, metadata.location, metadata.fileId, metadata.uuid, mdAttachment.attachmentName, 
-								typeInformationLabel.label, creatorLabel.label, metadata.creatorOther, rightsLabel.label, useLimitationLabel.label,
-								mdFormatLabel.label, metadata.source, subjectLabel.label))
-					.from(metadata)
-					.join(mdAttachment).on(metadata.id.eq(mdAttachment.metadataId))
-					.join(typeInformation).on(metadata.typeInformation.eq(typeInformation.id))
-	    			.join(typeInformationLabel).on(typeInformation.id.eq(typeInformationLabel.id))
-	    			.join(creator).on(metadata.creator.eq(creator.id))
-	    			.join(creatorLabel).on(creator.id.eq(creatorLabel.id))
-					.join(rights).on(metadata.rights.eq(rights.id))
-	    			.join(rightsLabel).on(rights.id.eq(rightsLabel.id))
-	    			.join(useLimitation).on(metadata.useLimitation.eq(useLimitation.id))
-	    			.join(useLimitationLabel).on(useLimitation.id.eq(useLimitationLabel.id))
-	    			.join(mdFormat).on(metadata.mdFormat.eq(mdFormat.id))
-	    			.join(mdFormatLabel).on(mdFormat.id.eq(mdFormatLabel.id))
-	    			.join(mdSubject).on(metadata.id.eq(mdSubject.metadataId))
-	    			.join(subject).on(mdSubject.subject.eq(subject.id))
-	    			.join(subjectLabel).on(subject.id.eq(subjectLabel.id))
-	    			.groupBy(metadata.id, metadata.title, metadata.description, metadata.location, metadata.fileId, metadata.uuid, typeInformationLabel.label,
-	    					creatorLabel.label, metadata.creatorOther, rightsLabel.label, useLimitationLabel.label, mdFormatLabel.label, metadata.source)
-					;
-		
-		List<Tuple> searchRecord = fullTextQuery.fetch();
-		for(Tuple searchField : searchRecord) {
-			System.out.println(searchField);
-		}
-		
-		if(!textSearchList.isEmpty()) {
-			for(String word : textSearchList) {
-				datasetQuery
-					.where(metadata.title.toLowerCase().containsIgnoreCase(word)
-						.or(metadata.description.toLowerCase().containsIgnoreCase(word))
-						.or(metadata.location.toLowerCase().containsIgnoreCase(word))
-						.or(metadata.fileId.toLowerCase().containsIgnoreCase(word))
-						.or(metadata.uuid.toLowerCase().containsIgnoreCase(word))
-						.or(mdAttachment.attachmentName.toLowerCase().containsIgnoreCase(word))
-						.or(typeInformationLabel.label.toLowerCase().containsIgnoreCase(word))
-						.or(creatorLabel.label.toLowerCase().containsIgnoreCase(word))
-						.or(metadata.creatorOther.toLowerCase().containsIgnoreCase(word))
-						.or(rightsLabel.label.toLowerCase().containsIgnoreCase(word))
-						.or(useLimitationLabel.label.toLowerCase().containsIgnoreCase(word))
-						.or(mdFormatLabel.label.toLowerCase().containsIgnoreCase(word))
-						.or(metadata.source.toLowerCase().containsIgnoreCase(word))
-						.or(subjectLabel.label.toLowerCase().containsIgnoreCase(word)));
-			}
-		}
-			
-			if(!supplierSearch.equals("none")) {
-				datasetQuery
-					.where(supplier.name.eq(supplierSearch));
-			}
-			
-			if(!statusSearch.equals("none")) {
-				datasetQuery
-					.where(status.name.eq(statusSearch));
-			}
-			
-			if(!mdFormatSearch.equals("none")) {
-				datasetQuery
-					.where(mdFormat.name.eq(mdFormatSearch));
-			}
-			
-			Timestamp timestampStartSearch = null;
-			Timestamp timestampEndSearch = null;
-			if(dateStartSearch != null && dateEndSearch != null) {
-				timestampStartSearch = new Timestamp(dateStartSearch.getTime());
-				timestampEndSearch = new Timestamp(dateEndSearch.getTime() + 86400000);
-				
-				datasetQuery
-					.where(metadata.lastRevisionDate.after(timestampStartSearch))
-					.where(metadata.lastRevisionDate.before(timestampEndSearch));
-			}
-			
-			List<Tuple> datasetRows = datasetQuery
-	    			.limit(200)
-					.orderBy(metadata.lastRevisionDate.desc())
-	    			.fetch();
-	    	
-	    	List<Tuple> supplierList = tx.select(supplier.all())
-	            	.from(supplier)
-	            	.fetch();
-	    	
-	    	List<Tuple> statusList = tx.select(status.name, statusLabel.label)
-	            	.from(status)
-	            	.join(statusLabel).on(status.id.eq(statusLabel.statusId))
-	            	.fetch();
-	    	
-	    	List<Tuple> mdFormatList = tx.select(mdFormat.name, mdFormatLabel.label)
-	            	.from(mdFormat)
-	            	.join(mdFormatLabel).on(mdFormat.id.eq(mdFormatLabel.mdFormatId))
-	            	.fetch();
-	    	
-	    	SimpleDateFormat sdfUS = new SimpleDateFormat("yyyy-MM-dd");
-	        SimpleDateFormat sdfLocal = new SimpleDateFormat("dd-MM-yyyy");
-	        
-	        Timestamp resetTimestampEndSearch = null;
-	        if(dateEndSearch != null) {
-	        	resetTimestampEndSearch = new Timestamp(dateEndSearch.getTime());
-	        }
-	    	
-			return ok(views.html.index.render(datasetRows, supplierList, statusList, mdFormatList, sdfUS, sdfLocal, textSearch, 
-					supplierSearch, statusSearch, mdFormatSearch, timestampStartSearch, resetTimestampEndSearch));
-		});
-	}
-	
->>>>>>> d40141a Added attachment and subject to text search query
 	public Boolean validateDate(String date) {
 		if("".equals(date)) {
 			return true;
