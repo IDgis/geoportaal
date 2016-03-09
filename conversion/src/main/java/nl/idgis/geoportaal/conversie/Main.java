c8896ffef821847a113e0a015e34126b767d6b2e
NijzinkM <mart.nijzink@idgis.nl>
2016-03-09 12:01:42 +0100

    add option to write to DB

--- conversion/src/main/java/nl/idgis/geoportaal/conversie/Main.java
+++ conversion/src/main/java/nl/idgis/geoportaal/conversie/Main.java
@@ -1,17 +1,17 @@
 package nl.idgis.geoportaal.conversie;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 
 public class Main {
 
 	private static final String DATABASE = "database";
 	private static final String PORT = "port";
-	private static final String SERVER_NAME = "serverName";
+	private static final String SERVER_NAME = "servername";
 	private static final String PASSWORD = "password";
 	private static final String USERNAME = "username";
 	private static final String CSV_DIR = "csvDir";
@@ -29,21 +29,52 @@ public class Main {
 		parser.addArgument("-" + PORT).type(Integer.class).help("poortnummer van de server");
 		parser.addArgument("-" + DATABASE).type(String.class).help("naam van de database");
 
-		Namespace res = parser.parseArgs(args);
+		File xmlDirectory = null;
+		File csvFile = null;
+		File txtFile = null;
+		String username = null;
+		String password = null;
+		String serverName = null;
+		Integer port = null;
+		String database = null;
 
-		File xmlDirectory = new File(res.getString(XML_DIR));
+		try {
+			Namespace res = parser.parseArgs(args);
 
-		File csvFile = new File(res.getString(CSV_DIR) + "/conversion_input.csv");
-		File txtFile = new File("./content_lengths.txt");
+			xmlDirectory = new File(res.getString(XML_DIR));
+			csvFile = new File(res.getString(CSV_DIR) + "/conversion_input.csv");
+			txtFile = new File("./content_lengths.txt");
+			username = res.getString(USERNAME);
+			password = res.getString(PASSWORD);
+			serverName = res.getString(SERVER_NAME);
+			port = res.getInt(PORT);
+			database = res.getString(DATABASE);
+		} catch (ArgumentParserException e) {
+            parser.handleError(e);
+            return;
+        }
 
-		Converter converter = new Converter(XML_ENCODING, new ToCSV(csvFile, txtFile,
-				"\"file_name\";"
-				+ "\"title\";\"creator\";\"subject\";\"description\";\"publisher\";"
-				+ "\"contributor\";\"date\";\"issued\";\"valid_start\";\"valid_end\";"
-				+ "\"type\";\"format\";\"identifier\";\"references\";\"relation_id\";"
-				+ "\"source\";\"language\";\"relation_attachment\";\"http_status_code\";"
-				+ "\"http_content_length\";\"rights_file\";\"rights_use\";\"temporal_start\";"
-				+ "\"temportal_end\";\"bbox_lowercorner\";\"bbox_uppercorner\""));
+		if (!xmlDirectory.exists() && !xmlDirectory.isDirectory())
+			throw new Exception(xmlDirectory.getPath() + " is niet geldig");
+
+		boolean toCsv = username == null && password == null && serverName == null && port == null && database == null;
+
+		OutDestination out;
+
+		if (toCsv) {
+			out = new ToCSV(csvFile, txtFile,
+					"\"file_name\";" + "\"title\";\"creator\";\"subject\";\"description\";\"publisher\";"
+							+ "\"contributor\";\"date\";\"issued\";\"valid_start\";\"valid_end\";"
+							+ "\"type\";\"format\";\"identifier\";\"references\";\"relation_id\";"
+							+ "\"source\";\"language\";\"relation_attachment\";\"http_status_code\";"
+							+ "\"http_content_length\";\"rights_file\";\"rights_use\";\"temporal_start\";"
+							+ "\"temportal_end\";\"bbox_lowercorner\";\"bbox_uppercorner\"");
+		} else {
+			out = new ToDB();
+			((ToDB) out).connect(username, password, serverName, port, database);
+		}
+
+		Converter converter = new Converter(XML_ENCODING, out);
 
 		converter.convertFiles(xmlDirectory.listFiles());
 	}
