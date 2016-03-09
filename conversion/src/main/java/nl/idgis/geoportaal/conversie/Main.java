e2ab88e76d7bf364b1b21414c709c5bf3f206420
NijzinkM <mart.nijzink@idgis.nl>
2016-03-09 11:35:45 +0100

    new argument parser

--- conversion/src/main/java/nl/idgis/geoportaal/conversie/Main.java
+++ conversion/src/main/java/nl/idgis/geoportaal/conversie/Main.java
@@ -3,14 +3,37 @@ package nl.idgis.geoportaal.conversie;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 public class Main {
 
+	private static final String DATABASE = "database";
+	private static final String PORT = "port";
+	private static final String SERVER_NAME = "serverName";
+	private static final String PASSWORD = "password";
+	private static final String USERNAME = "username";
+	private static final String CSV_DIR = "csvDir";
+	private static final String XML_DIR = "xmlDir";
 	private static final String XML_ENCODING = "UTF-8";
 
 	public static void main(String[] args) throws Exception {
-		File xmlDirectory = getFileFromArray(args, 0);
+		ArgumentParser parser = ArgumentParsers.newArgumentParser("conversion");
 
-		File csvFile = new File(getFileFromArray(args, 1) + "/conversion_input.csv");
+		parser.addArgument("-" + XML_DIR).dest(XML_DIR).type(String.class).help("map met XML-bestanden").required(true);
+		parser.addArgument("-" + CSV_DIR).type(String.class).help("map waar CSV-bestand met resultaten naar wordt geschreven (default: 'csvfiles')").setDefault("csvfiles");
+		parser.addArgument("-" + USERNAME).type(String.class).help("gebruikersnaam om in te loggen op de databaseserver");
+		parser.addArgument("-" + PASSWORD).type(String.class).help("wachtwoord om in te loggen op de databaseserver");
+		parser.addArgument("-" + SERVER_NAME).type(String.class).help("server waarop de database zich bevindt (bijvoorbeeld 'localhost'");
+		parser.addArgument("-" + PORT).type(Integer.class).help("poortnummer van de server");
+		parser.addArgument("-" + DATABASE).type(String.class).help("naam van de database");
+
+		Namespace res = parser.parseArgs(args);
+
+		File xmlDirectory = new File(res.getString(XML_DIR));
+
+		File csvFile = new File(res.getString(CSV_DIR) + "/conversion_input.csv");
 		File txtFile = new File("./content_lengths.txt");
 
 		Converter converter = new Converter(XML_ENCODING, new ToCSV(csvFile, txtFile,
@@ -23,17 +46,5 @@ public class Main {
 				+ "\"temportal_end\";\"bbox_lowercorner\";\"bbox_uppercorner\""));
 
 		converter.convertFiles(xmlDirectory.listFiles());
-	}
-
-	private static File getFileFromArray(String[] array, Integer numberParam) throws FileNotFoundException {
-		if (array.length != 2)
-			throw new IllegalArgumentException("aantal argumenten is niet gelijk aan 2");
-
-		File file = new File(array[numberParam]);
-
-		if (!file.exists())
-			throw new FileNotFoundException(file.getAbsolutePath());
-
-		return file;
 	}
 }
