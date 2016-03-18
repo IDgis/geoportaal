7261a236a4557a68af6dd7e046900b8b446e6217
Sandro Neumann <sandro.neumann@idgis.nl>
2016-03-18 12:00:30 +0100

    Implemented attachment check on content length

--- conversion/src/main/java/nl/idgis/geoportaal/conversie/Attachment.java
+++ conversion/src/main/java/nl/idgis/geoportaal/conversie/Attachment.java
@@ -22,11 +22,14 @@ public class Attachment implements Closeable {
 		final int responseCode = connection.getResponseCode();
 		if (responseCode != 200)
 			throw new Exception("request naar " + url + " stuurde code " + responseCode + " terug");
+		
+		Long contentLength = connection.getContentLengthLong();
+		if(contentLength.intValue() > 214958080) {
+			throw new Exception("bijlage van " + url + " is te groot");
+		}
 
-		final String contentType = connection.getContentType();
-
-		attachment.setLength(connection.getContentLengthLong());
-		attachment.setMimeType(contentType.substring(contentType.indexOf('/') + 1));
+		attachment.setLength(contentLength);
+		attachment.setMimeType(connection.getContentType());
 		attachment.setFileName(url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.')));
 		attachment.setDataStream(connection.getInputStream());
 
