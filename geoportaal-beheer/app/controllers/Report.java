1a1b5304e51d947ba1e3f774c2ee69344bcc2d1d
Sandro Neumann <sandro.neumann@idgis.nl>
2016-05-20 16:26:12 +0200

    Only show report tab when user is an admin

--- geoportaal-beheer/app/controllers/Report.java
+++ geoportaal-beheer/app/controllers/Report.java
@@ -1,12 +1,18 @@
 package controllers;
 
+import static models.QUser.user;
+
+import javax.inject.Inject;
+
 import actions.DefaultAuthenticator;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import util.QueryDSL;
 
 @Security.Authenticated(DefaultAuthenticator.class)
 public class Report extends Controller {
+	@Inject QueryDSL q;
 	
 	/**
 	 * The rendering of the report page
@@ -14,6 +20,14 @@ public class Report extends Controller {
 	 * @return the {@link Result} of the report page
 	 */
 	public Result renderReport() {
-		return ok(views.html.report.render());
+		return q.withTransaction(tx -> {
+			// Fetches the role of the logged in user
+			Integer roleId = tx.select(user.roleId)
+				.from(user)
+				.where(user.username.eq(session("username")))
+				.fetchOne();
+			
+			return ok(views.html.report.render(roleId));
+		});
 	}
 }
