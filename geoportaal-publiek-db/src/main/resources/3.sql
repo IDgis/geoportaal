5474a01cb68dd671abf941cb9a65e43f4f3ed60f
Sandro Neumann <sandro.neumann@idgis.nl>
2016-09-16 10:30:05 +0200

    added published column to public portal database

--- geoportaal-publiek-db/src/main/resources/3.sql
+++ geoportaal-publiek-db/src/main/resources/3.sql
@@ -2,8 +2,10 @@
 
 ALTER TABLE gp.document ADD COLUMN downloadable boolean;
 ALTER TABLE gp.document ADD COLUMN spatial_schema text;
+ALTER TABLE gp.document ADD COLUMN published boolean;
 
 # --- !Downs
 
 ALTER TABLE gp.document DROP COLUMN downloadable;
 ALTER TABLE gp.document DROP COLUMN spatial_schema;
+ALTER TABLE gp.document DROP COLUMN published;
