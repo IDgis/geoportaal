7ae16673f812abd66de4b4242d93c065abe20b1c
Sandro Neumann <sandro.neumann@idgis.nl>
2016-09-28 15:07:54 +0200

    extracted environment specific data out of sql & moved all sql in 1 file

--- geoportaal-beheer/conf/evolutions/default/17.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-
-ALTER TABLE gb.metadata DROP COLUMN date_source_revision;
-
-# --- !Downs
-
-ALTER TABLE gb.metadata ADD COLUMN date_source_revision timestamp;
--- geoportaal-beheer/conf/evolutions/default/6.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-
-UPDATE gb.status_label SET label = 'In prullenbak' WHERE id = 5;
-
-# --- !Downs
-
-UPDATE gb.status_label SET label = 'In prullebak' WHERE id = 5;
