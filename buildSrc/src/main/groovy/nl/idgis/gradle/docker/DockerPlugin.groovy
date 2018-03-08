146d86c95d7c6315cdba19cf00e021564d3af666
Sandro Neumann <sandro.neumann@idgis.nl>
2018-03-08 14:13:06 +0100

    Change dockerplugin so playBinary zips are unzipped in docker directory

--- buildSrc/src/main/groovy/nl/idgis/gradle/docker/DockerPlugin.groovy
+++ buildSrc/src/main/groovy/nl/idgis/gradle/docker/DockerPlugin.groovy
@@ -42,25 +42,25 @@ class DockerPlugin implements Plugin<Project> {
 					play {
 						binaries.all { binary ->
 							def distTask = "create${binary.name.capitalize()}Dist"
-							def copyBinaryTask = "copy${binary.name.capitalize()}Docker"
+							def copyUnzipBinaryTask = "copy${binary.name.capitalize()}Docker"
 							def createDockerfileTask = "create${binary.name.capitalize()}Dockerfile"
 							def buildImageTask = "build${binary.name.capitalize()}DockerImage"
 							def copyScriptTask = "copy${binary.name.capitalize()}DockerStartupScript"
-
-							// Copy the Play binary ZIP to the directory where the Docker build context
-							// is materialized:
-							tasks.create (copyBinaryTask, Copy) { task ->
+							
+							// Copy and unzip the Play binary ZIP to the directory where the Docker build 
+							// context is materialized:
+							tasks.create (copyUnzipBinaryTask, Copy) { task ->
 								task.dependsOn distTask
 								
-								from "${project.buildDir}/distributions/${binary.name}.zip"
+								from project.zipTree("${project.buildDir}/distributions/${binary.name}.zip")
 								into "${project.buildDir}/docker/${binary.name}"
 							}
-
+							
 							// Write a custom startup script to the Docker context directory. This
 							// script takes care of adding environment variables as configuration
 							// settings to the Play process:
 							tasks.create (copyScriptTask, Task) { task ->
-								task.dependsOn copyBinaryTask
+								task.dependsOn copyUnzipBinaryTask
 								
 								def content = this.getClass ().getResource ("/nl/idgis/gradle/docker/start-application.sh").text
 								def output = project.file "${project.buildDir}/docker/${binary.name}/start-application.sh"
@@ -71,17 +71,16 @@ class DockerPlugin implements Plugin<Project> {
 									output.write (content)
 								}
 							}
-
+							
 							// Generate a Dockerfile in the context directory:
 							tasks.create (createDockerfileTask, Dockerfile) { task ->
 								task.destFile = project.file "${project.buildDir}/docker/${binary.name}/Dockerfile"
 								task.dependsOn distTask
 								
 								task.from "azul/zulu-openjdk:8"
-								task.copyFile ("${binary.name}.zip", "/opt/${binary.name}.zip")
+								task.copyFile ("${binary.name}", "/opt/${binary.name}")
 								task.copyFile ("start-application.sh", "/opt/start-application.sh")
-								task.runCommand "unzip /opt/${binary.name}.zip -d /opt/" +
-									" && chown -R daemon:daemon /opt/${binary.name}" +
+								task.runCommand "chown -R daemon:daemon /opt/${binary.name}" +
 									" && chmod +x /opt/${binary.name}/bin/${binary.name}" +
 									" && mkdir /var/lib/${binary.name}" + 
 									" && chown -R daemon:daemon /var/lib/${binary.name}" +
@@ -94,13 +93,13 @@ class DockerPlugin implements Plugin<Project> {
 								task.user "daemon"
 								task.defaultCommand ("/opt/start-application.sh", "/opt/${binary.name}/bin/${binary.name}") 
 							}
-							 
+							
 							// Build the Docker image:
 							tasks.create (buildImageTask, DockerBuildImage) { task->
 								task.dependsOn createDockerfileTask
-								task.dependsOn copyBinaryTask
+								task.dependsOn copyUnzipBinaryTask
 								task.dependsOn copyScriptTask
-			
+								
 								def tagVersion = "${project.version}".replaceAll ("\\+", "-")
 								
 								task.inputDir = project.file "${project.buildDir}/docker/${binary.name}"
