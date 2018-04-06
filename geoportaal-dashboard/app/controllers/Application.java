c0056bcadba4846f36d8070ac202765fb4d37f31
Sandro Neumann <sandro.neumann@idgis.nl>
2018-04-06 09:38:09 +0200

    Implement main.html

--- geoportaal-dashboard/app/controllers/Application.java
+++ geoportaal-dashboard/app/controllers/Application.java
@@ -1,12 +1,18 @@
 package controllers;
 
-import play.mvc.*;
+import javax.inject.Inject;
 
-import views.html.*;
+import play.Configuration;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
-
+	@Inject Configuration config;
+	
 	public Result index() {
-		return ok(index.render("It works"));
+		final String logo = config.getString("dashboard.client.logo");
+		
+		return ok(index.render(logo));
 	}
 }
