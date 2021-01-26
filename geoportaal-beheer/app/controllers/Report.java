32c87c54a95e8bceacc7b33cf6a51789b84a2d80
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2021-01-26 14:26:20 +0100

    renamed column count to count_number

--- geoportaal-beheer/app/controllers/Report.java
+++ geoportaal-beheer/app/controllers/Report.java
@@ -152,7 +152,7 @@ public class Report extends Controller {
 				getContentDispositionValue("dublincore"));
 		
 		String header = "\"title\";\"creator\";\"subject\";\"description\";\"date_creation\";\"date_publication\";\"date_valid_start\";"
-				+ "\"date_valid_end\";\"type\";\"format\";\"identifier\";\"location\";\"number\";\"count\";\"source\";\"attachment\";"
+				+ "\"date_valid_end\";\"type\";\"format\";\"identifier\";\"location\";\"number\";\"count_number\";\"source\";\"attachment\";"
 				+ "\"attachment_size_in_mb\";\"attachment_size_in_mb_total\";\"rights\";\"use_limitation\";\"supplier\";\"role_supplier\";"
 				+ "\"status\";\"last_revision_user\";\"last_revision_date\";\"publisher\";\"contributor\";\"language\";\"west_bound\";"
 				+ "\"east_bound\";\"south_bound\";\"north_bound\";";
